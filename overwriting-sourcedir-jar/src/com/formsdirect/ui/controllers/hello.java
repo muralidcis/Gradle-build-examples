6aa98c1fbd457f7396db5a93ba19ac74319ee344
muralidcis <murali@dcis.net>
2015-04-09 17:04:40 +0530

    more updates to the gradle build, addition of git version, addition of jars lib

--- overwriting-sourcedir-jar/src/com/formsdirect/ui/controllers/hello.java
+++ overwriting-sourcedir-jar/src/com/formsdirect/ui/controllers/hello.java
@@ -1,7 +1,11 @@
 package com.formsdirect.ui.controllers;
 import java.io.*;
+import org.joda.time.LocalTime;
+
 public class hello{
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+	LocalTime currentTime = new LocalTime();
+	System.out.println("The current local time is: " + currentTime);
+
     }
 }
